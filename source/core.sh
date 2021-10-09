8f8a29a1bf6789c9f780de79dd14551c6545f3f8
Morteza Jamali <mortezajamali4241@gmail.com>
2021-10-09 07:06:29 +0000

    feat(core): add prompt for 'recover' command

--- source/core.sh
+++ source/core.sh
@@ -28,33 +28,30 @@ add_meta() {
 
 # List all removed objects
 list_objects() {
+  OPTION="--all"
+  no_data=true
+
   echo "Hash | Date | Path"
 
   if [[ $(wc -l < $META_FILE_PATH) != "0" ]]; then
     while IFS="," read -r r_date r_hash r_path; do
       if [[ ! -z "$r_path" && ! -z "$r_date" && ! -z "$r_hash" ]]; then
-        echo "$r_hash | $r_date | $r_path" | xargs
+        if [ "${1^^}" = "${OPTION^^}" ]; then
+          no_data=false
+          echo "$r_hash | $r_date | $r_path" | xargs
+        else
+          if [ "$1" = "$(echo -n $r_path | xargs)" -o "$1" = "$(echo -n $r_hash | xargs)" ]; then
+            no_data=false
+            echo "$r_hash | $r_date | $r_path" | xargs
+          fi
+        fi
       fi
     done < $META_FILE_PATH
-  else
+  fi
+
+  if [ "$no_data" = "true" ]; then
     echo "--- No data avalable ---"
   fi
-}
-
-# Recover removed objects
-recover_objects() {
-  echo $1
-  # if [ -d "$1" || -f "$1" ]; then
-  #   echo "haha"
-  # else
-  #   echo "File or directory doesn't exist !"
-  #   exit 1
-  # fi
-}
-
-# Show cli help
-show_help() {
-  rm --help
 }
 
 # Remove object(s) permanently
@@ -77,4 +74,33 @@ clear_objects() {
       done < $META_FILE_PATH
     ;;
   esac
+}
+
+# Recover removed objects
+recover_objects() {
+  OPTION="--all"
+  occurrence_count=$(grep -c "$1" $META_FILE_PATH)
+
+  if [ "$occurrence_count" = "0" ]; then
+    echo "Object '$1' doesn't exist in recycle bin !" && exit 1
+  fi
+
+  if [ "$occurrence_count" -gt 1 ]; then
+    echo "Which one do you want to recover ?(hash)" && echo && list_objects $1
+    exit 1
+  fi
+
+  while IFS="," read -r r_date r_hash r_path; do
+    if [[ ! -z "$r_path" && ! -z "$r_date" && ! -z "$r_hash" ]]; then
+      if [ $1 = $r_path -o $1 = $r_hash ]; then
+        mv $BACKUP_DIR_PATH/$(echo -n $r_hash | xargs)/$(basename $r_path)/* $r_path && \
+        clear_objects $r_hash
+      fi
+    fi
+  done < $META_FILE_PATH
+}
+
+# Show cli help
+show_help() {
+  rm --help
 }
